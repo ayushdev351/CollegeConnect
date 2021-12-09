818b71fd039422c3631c32540ae53f69aefc4962
Ayush Chauhan <79743704+ayushdev351@users.noreply.github.com>
2021-12-09 23:20:25 +0530

    Remove Warnings for Deployment

--- src/components/MainScreen/Components/Spaces.js
+++ src/components/MainScreen/Components/Spaces.js
@@ -18,9 +18,6 @@ function Spaces() {
     getUsers();
   });
 
-  const color =
-    "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
-
   return (
     <>
       <SpaceStyle>
